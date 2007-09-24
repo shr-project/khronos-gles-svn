f91b7ff01244f7ec4bad5592dc9d94a99439623f
oddhack <oddhack@e7fa87d3-cd2b-0410-9028-fcbf551c1848>
2007-09-24 18:34:04 +0000

    git-svn-id: https://cvs.khronos.org/svn/repos/registry/trunk/public/gles@4093 e7fa87d3-cd2b-0410-9028-fcbf551c1848

--- index.php
+++ index.php
@@ -159,6 +159,10 @@
 </li>
 <li value=38> <a href="extensions/OES/OES_vertex_half_float.txt">GL_OES_vertex_half_float</a>
 </li>
+<li value=39> <a href="extensions/AMD/AMD_compressed_3DC_texture.txt">GL_AMD_compressed_3DC_texture.txt</a>
+</li>
+<li value=40> <a href="extensions/AMD/AMD_compressed_ATC_texture.txt">GL_AMD_compressed_ATC_texture.txt</a>
+</li>
 </ol>
 <?php include("/home/virtual/khronos.org/var/www/html/_inc/registry/registry_footer.txt"); ?>
 </body>  <!-- END pagewrapper -->
