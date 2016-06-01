c6941b77a3c0b5e420fc63e9a502221a2ecf4933
oddhack <oddhack@e7fa87d3-cd2b-0410-9028-fcbf551c1848>
2016-06-01 00:13:51 +0000

    git-svn-id: https://cvs.khronos.org/svn/repos/registry/trunk/public/gles@32906 e7fa87d3-cd2b-0410-9028-fcbf551c1848

--- index.php
+++ index.php
@@ -834,6 +834,8 @@ include_once("../../assets/static_pages/khr_page_top.php");
 </li>
 <li value=261> <a href="extensions/NV/shader_atomic_fp16_vector.txt">GL_NV_shader_atomic_fp16_vector</a>
 </li>
+<li value=262> <a href="extensions/NV/conservative_raster_pre_snap_triangles.txt">GL_NV_conservative_raster_pre_snap_triangles</a>
+</li>
 </ol>
 <?php include_once("../../assets/static_pages/khr_page_bottom.php"); ?>
 </body>
